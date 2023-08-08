6544d3013ccdcec205435f6780fece2fbc0cf483
lu4ik-dev <60878215+lu4ik-dev@users.noreply.github.com>
2023-08-08 03:21:21 +0300

    first commit with react js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SwiperCore from './components/mySwiper';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SwiperCore></SwiperCore>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mySwiper.js
@@ -0,0 +1,11 @@
+import { Virtual } from 'swiper/modules';
+import VerticalSlide from './VerticalSlide';
+import HorizontalSlide from './HorizontalSlide';
+
+export { Swiper, SwiperSlide };
+// Install Swiper modules
+// Import Swiper styles
+
+export default () => {
+  return <Swiper></Swiper>;
+};
