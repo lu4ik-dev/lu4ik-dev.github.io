74ef0dfa100a54ae6c82b0614b3b402b85d1329f
lu4ik-dev <60878215+lu4ik-dev@users.noreply.github.com>
2023-08-17 20:53:12 +0300

    fix index.js routing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,7 +8,12 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 );
 
