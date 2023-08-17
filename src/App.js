2adb0d39823c4dadc9f2cf84b6b000c2119264c4
lu4ik-dev <60878215+lu4ik-dev@users.noreply.github.com>
2023-08-17 22:17:18 +0300

    language upd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min';
 import './App.css';
@@ -6,6 +7,8 @@ import Sections from './components/js/sections';
 import ModalChangeLanguage from './components/js/modalChangeLanguage';
 import { fetchAndParseJSON, changeLanguage } from './components/js/changeLanguage';
 import OverlayBottomButtons from './components/js/overlayBottomButtons';
+import Projects from './components/js/Projects';
+import slidesData from './projects.json';
 
 const jsonUrl =
   'https://raw.githubusercontent.com/lu4ik-dev/lu4ik-dev.github.io/main/languages.json';
@@ -31,10 +34,12 @@ function App() {
   };
 
   return (
-    <div>
-      <ModalChangeLanguage onSelectLanguage={handleLanguageChange} languageData={languageData} />
-      <Sections languageData={languageData} />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Sections languageData={languageData} />} />
+        <Route path="/projects" element={<Projects slides={slidesData} />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/js/sections/Slide1.js
+++ src/components/js/sections/Slide1.js
@@ -1,46 +1,68 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import SpanTitles from '../SpanTitles';
+import ImageComputer from '../../images/computer';
+const Slide1 = ({ languageData }) => {
+  const greetingTitleRef = useRef(null);
 
-const Slide1 = ({ languageData }) => (
-  <header class="bg-gradient-start vh-100">
-    <div class="position-absolute w-100 py-3 mb-4">
-      <div class="container-fluid p-0 px-lg-2 d-flex flex-wrap justify-content-center">
-        <a
-          href="https://lu4ik-dev.github.io/"
-          class="d-flex align-items-center ms-lg-3 mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
-          <img src="assets/elis.png" class="logo-img my-auto" alt="logo" />
-          <h1 class="fs-4 logo-add-text text-light my-auto">
-            <span>D</span>
-            <span>m</span>
-            <span>i</span>
-            <span>t</span>
-            <span>r</span>
-            <span>i</span>
-            <span>y</span>
-            <span> </span>
-            <span>S</span>
-            <span>m</span>
-            <span>i</span>
-            <span>r</span>
-            <span>n</span>
-            <span>o</span>
-            <span>v</span>
-          </h1>
-        </a>
-        <a class="mx-2 py-2 my-auto btn btn-outline-light fw-bolder" href="#" id="btn-contact">
-          {languageData.btnContactLabel}
-        </a>
-        <button
-          type="button"
-          class="btn btn-outline-light "
-          data-bs-toggle="modal"
-          data-bs-target="#modal-dialog-change-language">
-          <i class="fs-4 mx-2 my-auto fa fa-language" aria-hidden="true"></i>
-        </button>
+  useEffect(() => {
+    const greetingTitleElement = greetingTitleRef.current;
+
+    if (greetingTitleElement) {
+      SpanTitles(languageData.greetingTitle, greetingTitleElement);
+    }
+  }, [languageData]);
+  return (
+    <header class="bg-gradient-start vh-100">
+      <div class="position-absolute w-100 py-3 mb-4">
+        <div class="container-fluid p-0 px-lg-2 d-flex flex-wrap justify-content-center">
+          <a
+            href="https://lu4ik-dev.github.io/"
+            class="d-flex align-items-center ms-lg-3 mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+            <img src="assets/elis.png" class="logo-img my-auto" alt="logo" />
+            <h1 class="fs-4 logo-add-text text-light my-auto">
+              <span>D</span>
+              <span>m</span>
+              <span>i</span>
+              <span>t</span>
+              <span>r</span>
+              <span>i</span>
+              <span>y</span>
+              <span> </span>
+              <span>S</span>
+              <span>m</span>
+              <span>i</span>
+              <span>r</span>
+              <span>n</span>
+              <span>o</span>
+              <span>v</span>
+            </h1>
+          </a>
+          <a class="mx-2 py-2 my-auto btn btn-outline-light fw-bolder" href="#" id="btn-contact">
+            {languageData.btnContactLabel}
+          </a>
+          <button
+            type="button"
+            class="btn btn-outline-light "
+            data-bs-toggle="modal"
+            data-bs-target="#modal-dialog-change-language">
+            <i class="fs-4 mx-2 my-auto fa fa-language" aria-hidden="true"></i>
+          </button>
+        </div>
       </div>
-    </div>
 
-    <div></div>
-  </header>
-);
-
+      <div class="container px-5">
+        <div class="row gx-5 align-items-center justify-content-center">
+          <div class="col-lg-8 col-xl-7 col-xxl-6">
+            <h1 class="display-5 fw-bolder text-white mb-2" ref={greetingTitleRef}>
+              {languageData.greetingTitle}
+            </h1>
+          </div>
+          <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
+            <ImageComputer width="420" height="420" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 export default Slide1;
