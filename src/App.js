f1ae1b600f3e50152e2ea96c38291ab2b58ac934
lu4ik-dev <60878215+lu4ik-dev@users.noreply.github.com>
2023-08-18 23:03:20 +0300

    test rf

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min';
 import './App.css';
 import Sections from './components/js/sections';
 import ModalChangeLanguage from './components/js/modalChangeLanguage';
 import { fetchAndParseJSON, changeLanguage } from './components/js/changeLanguage';
-import Projects from './components/js/Projects';
-import slidesData from './projects.json';
 
 const jsonUrl =
   'https://raw.githubusercontent.com/lu4ik-dev/lu4ik-dev.github.io/main/languages.json';
@@ -43,12 +40,7 @@ function App() {
   return (
     <div>
       <ModalChangeLanguage onSelectLanguage={handleLanguageChange} languageData={languageData} />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Sections languageData={languageData} />} />
-          <Route path="/projects" element={<Projects slides={slidesData} />} />
-        </Routes>
-      </Router>
+      <Sections languageData={languageData} />
     </div>
   );
 }
