3e98600850259cf5d97da4ba33a7a1bda1554210
lu4ik-dev <60878215+lu4ik-dev@users.noreply.github.com>
2023-08-25 02:06:56 +0300

    skills json

--- src/components/js/sections/Slide3.js
+++ src/components/js/sections/Slide3.js
@@ -1,9 +1,15 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import SpanTitles from '../SpanTitles';
 import '../../css/slide3.css';
 import Fullpage, { FullpageContext } from '@ap.cx/react-fullpage';
+import { fetchAndParseJSON } from '../changeLanguage';
+
+const jsonUrl =
+  'https://raw.githubusercontent.com/lu4ik-dev/lu4ik-dev.github.io/main/skillsAndExperience.json';
 
 const Slide3 = ({ languageData }) => {
+  const [dataSkills, setDataSkills] = useState([]);
+
   const skillsTitleLabelRef = useRef(null);
 
   useEffect(() => {
@@ -12,28 +18,38 @@ const Slide3 = ({ languageData }) => {
     if (skillsTitleElement) {
       SpanTitles(languageData.skillsTitleText, skillsTitleElement);
     }
+
+    const fetchData = async () => {
+      const skillsData = await fetchAndParseJSON(jsonUrl);
+      setDataSkills(skillsData);
+    };
+    fetchData();
   }, [languageData]);
 
   return (
     <div class="bg-gradient-third py-5 vh-100">
       <div class="container px-5">
         <div class="row gx-5 align-items-center justify-content-center">
-          <h1 class="display-5 fw-bolder text-white mb-2" ref={skillsTitleLabelRef}>
-            {}
-          </h1>
-          <p class="fw-normal text-white-50 mb-4 fs-3">
-            <i data-bs-toggle="tooltip" title="Java Script" class="fab fa-js"></i>
-            <i data-bs-toggle="tooltip" title="CSS" class="fab fa-css3-alt"></i>
-            <i data-bs-toggle="tooltip" title="HTML" class="fab fa-html5"></i>
-            <i data-bs-toggle="tooltip" title="PHP" class="fab fa-php"></i>
-            <i data-bs-toggle="tooltip" title="Windows" class="fab fa-windows"></i>
-            <i data-bs-toggle="tooltip" title="Linux" class="fab fa-linux"></i>
-            <i data-bs-toggle="tooltip" title="Node.JS" class="fab fa-node-js"></i>
-            <i data-bs-toggle="tooltip" title="MySQL" class="fa-solid fa-database"></i>
-            <i data-bs-toggle="tooltip" title="Python" class="fab fa-python"></i>
-            <i data-bs-toggle="tooltip" title="Github" class="fab fa-github"></i>
-            <i data-bs-toggle="tooltip" title="Git" class="fab fa-git"></i>
-          </p>
+          <h1 class="display-5 fw-bolder text-white mb-2" ref={skillsTitleLabelRef}></h1>
+          <div className="container">
+            <div className="box">
+              <div className="shadow" />
+              <div className="content">
+                <div className="percent" data-text="HTML" style={{ '--num': 85 }}>
+                  <div className="dot" />
+                  <svg>
+                    <circle cx={70} cy={70} r={70} />
+                    <circle cx={70} cy={70} r={70} />
+                  </svg>
+                </div>
+                <div className="number">
+                  <h2>
+                    85<span>%</span>
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
             <FullpageContext.Consumer>
               {(ctx) => (
@@ -41,7 +57,7 @@ const Slide3 = ({ languageData }) => {
                   class="btn btn-outline-light btn-lg px-4 me-sm-3"
                   onClick={() => ctx.goto(ctx.slides[3], true)}
                   onPress={() => ctx.goto(ctx.slides[3], true)}>
-                  View projects (in dev)
+                  View projects
                 </a>
               )}
             </FullpageContext.Consumer>
